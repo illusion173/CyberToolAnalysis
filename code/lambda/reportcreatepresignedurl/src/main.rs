c04ce4ce7a8b2ad6c5d93d75bd6cec820889cf86
Jeremiah Webb <illusion173@hotmail.com>
2023-10-15 15:41:20 -0400

    Updated Rust functions with Rust

--- code/lambda/reportcreatepresignedurl/src/main.rs
+++ code/lambda/reportcreatepresignedurl/src/main.rs
@@ -3,6 +3,7 @@ use aws_sdk_s3::Client as S3Client;
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 use serde::{Deserialize, Serialize};
 use std::time::Duration;
+
 #[derive(Deserialize)]
 struct Request {
     bucket: String,
@@ -24,16 +25,19 @@ struct Response {
 }
 
 async fn get_presigned_request(client_opts: &Opts) -> Result<String, Error> {
+    // Get config from env
     let config = aws_config::load_from_env().await;
+    // Create a new s3 client
     let client = S3Client::new(&config);
     let expires_in = Duration::from_secs(client_opts.expires_in);
+    // Generate request & send, await feedback
     let presigned_request = client
         .get_object()
         .bucket(&client_opts.bucket)
         .key(&client_opts.object)
         .presigned(PresigningConfig::expires_in(expires_in)?)
         .await?;
-
+    // When presigned_url dont, get url convert to string.
     let url = presigned_request.uri().to_string();
     Ok(url)
 }
@@ -46,7 +50,7 @@ async fn function_handler(event: LambdaEvent<Request>) -> Result<Response, Error
         expires_in: event.payload.time,
     };
 
-    let presigned_url = get_presigned_request(&client_opts).await.unwrap();
+    let presigned_url = get_presigned_request(&client_opts).await?;
 
     // Return Response, contains url for physical object in s3
     let resp = Response {
--- code/lambda/reportcybertool/src/main.rs
+++ code/lambda/reportcybertool/src/main.rs
@@ -1,6 +1,12 @@
+use aws_config;
+use aws_sdk_s3 as S3;
+use genpdf;
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 use serde::{Deserialize, Serialize};
+use std::fs;
 
+const TEMPDIR: &str = "/tmp";
+const FONTSDIR: &str = "/tmp/fonts";
 #[derive(Deserialize)]
 struct Request {
     status_code: u64,
@@ -13,6 +19,53 @@ struct Response {
     object: String,
 }
 
+async fn save_file() -> Result<(), Error> {
+    Ok(())
+}
+
+async fn create_directories() -> Result<(), Error> {
+    // Change the current working directory to /tmp
+    std::env::set_current_dir(TEMPDIR.to_owned())?;
+    // Create the /tmp/fonts directory
+    fs::create_dir_all(FONTSDIR.to_owned())?;
+    Ok(())
+}
+
+async fn load_fonts() -> Result<(), Error> {
+    create_directories().await?;
+    // Create client
+    let config = aws_config::load_from_env().await;
+    let s3client = aws_sdk_s3::Client::new(&config);
+
+    // Set S3 bucket of location of font file
+    // Set object key
+    //
+
+    Ok(())
+}
+
+async fn make_file() -> Result<(), Error> {
+    // Load a font from the file system
+    load_fonts().await?;
+    let font_family = genpdf::fonts::from_files(FONTSDIR, "LiberationSans", None)?;
+    // Create a document and set the default font family
+    let mut doc = genpdf::Document::new(font_family);
+    doc.set_title("Demo document");
+    // Customize the pages
+    let mut decorator = genpdf::SimplePageDecorator::new();
+
+    decorator.set_margins(10);
+
+    doc.set_page_decorator(decorator);
+    // Add one or more elements
+    doc.push(genpdf::elements::Paragraph::new("This is a demo document."));
+    // Render the document and write it to a file
+    doc.render_to_file("output.pdf")?;
+    save_file().await?;
+
+    Ok(())
+}
+
 async fn function_handler(event: LambdaEvent<Request>) -> Result<Response, Error> {
     // Prepare the response
     let resp = Response {
@@ -20,6 +73,10 @@ async fn function_handler(event: LambdaEvent<Request>) -> Result<Response, Error
         bucket: "TEST".to_owned(),
         object: "TESTOBJECT".to_owned(),
     };
+
+    make_file().await?;
+
+    save_file().await?;
 
     Ok(resp)
 }
