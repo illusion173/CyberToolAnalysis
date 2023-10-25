8c029de48866069ae441ef122b76e5efa85f2ac6
Jeremiah Webb <illusion173@hotmail.com>
2023-10-25 11:51:12 -0400

    Updated Begin Report code

--- code/lambda/beginreportcybertool/src/main.rs
+++ code/lambda/beginreportcybertool/src/main.rs
@@ -1,39 +1,19 @@
 use aws_sdk_sfn as sfn;
-use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
+use lambda_http::{run, service_fn, Body, Error, Request, Response};
 use serde::{Deserialize, Serialize};
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-///
-///
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct FileData {
     file_name: String,
     cognito_user_data: String,
 }
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    // Extract some useful information from the request
-    let file_name = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("file_name"))
-        .unwrap();
-    let cognito_user_data = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("cognito_user_data"))
-        .unwrap();
-
-    let file_data_struct = FileData {
-        file_name: file_name.to_string(),
-        cognito_user_data: cognito_user_data.to_string(),
-    };
+    // Get the body of the request
+    let body_str = std::str::from_utf8(event.body().as_ref()).unwrap();
+    let file_data_struct: FileData = serde_json::from_str(body_str).unwrap();
 
     let serialized_file_data_struct = serde_json::to_string(&file_data_struct).unwrap();
 
-    let message = "SUCCESS! Report Being Created Now.".to_string();
-
     // Begin Report Step Function
-
     let config = aws_config::load_from_env().await;
     let sfn_client = sfn::Client::new(&config);
     sfn_client
@@ -45,6 +25,7 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
         .send()
         .await?;
 
+    let message = "SUCCESS! Report Being Created Now.".to_string();
     // Return something that implements IntoResponse.
     // It will be serialized to the right response event automatically by the runtime
     let resp = Response::builder()
