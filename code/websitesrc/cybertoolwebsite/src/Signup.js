1db1fab6f649cf0159663384393cc2406dff1173
meholico <meholico@my.erau.edu>
2023-11-09 15:07:06 -0500

    Sign up page format

--- code/websitesrc/cybertoolwebsite/src/Signup.js
+++ code/websitesrc/cybertoolwebsite/src/Signup.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Dashboard from "./Dashboard";
 import { useNavigate } from "react-router-dom"
+import './Signup.css';
 
 function Signup() {
     const navigate = useNavigate();
