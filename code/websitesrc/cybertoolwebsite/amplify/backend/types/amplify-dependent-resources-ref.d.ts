3a44f79ddfbad5e35d44c3dccb9921e7fe9a2c2b
Jeremiah Webb <illusion173@hotmail.com>
2023-12-03 16:13:08 -0500

    Really fixed amplify

--- code/websitesrc/cybertoolwebsite/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ code/websitesrc/cybertoolwebsite/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -8,10 +8,6 @@ export type AmplifyDependentResourcesAttributes = {
             "UserPoolName": "string",
             "AppClientIDWeb": "string",
             "AppClientID": "string"
-        },
-        "userPoolGroups": {
-            "GuestUsersGroupRole": "string",
-            "authenticatedGroupRole": "string"
         }
     },
     "function": {
