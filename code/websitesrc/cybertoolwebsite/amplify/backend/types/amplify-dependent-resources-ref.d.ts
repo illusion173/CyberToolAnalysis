c419b024882532271f5284c82b41957a291fdb36
Jeremiah Webb <illusion173@hotmail.com>
2023-12-04 12:24:34 -0500

    Readded Auth

--- code/websitesrc/cybertoolwebsite/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ code/websitesrc/cybertoolwebsite/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,15 @@
 export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "cybertoolwebsite90a2f0f3": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
     "function": {
         "cybertoolwebsitef06de9f8": {
             "Name": "string",
