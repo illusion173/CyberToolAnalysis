a878c835cd3f14bc90e75d859c7ba0f2ee31b350
Jeremiah Webb <illusion173@hotmail.com>
2024-01-11 11:26:23 -0500

    Redoing Auth

--- code/websitesrc/cybertoolwebsite/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ code/websitesrc/cybertoolwebsite/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "cybertoolwebsite82b62747": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
