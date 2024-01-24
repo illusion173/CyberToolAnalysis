fc0e7c25ffef0c4d77a7aa36de7ef4ac8ac8aa94
ChanChoOG <nicolas.rod247@gmail.com>
2024-01-24 11:51:52 -0500

    Updated to main... no changes

--- code/websitesrc/cybertoolwebsite/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ code/websitesrc/cybertoolwebsite/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,29 +1,29 @@
 export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "apiab9b8614": {
-            "RootUrl": "string",
-            "ApiName": "string",
-            "ApiId": "string"
-        }
-    },
-    "auth": {
-        "cybertoolwebsite82b62747": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
-    "function": {
-        "cybertoolwebsite430c7922": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string",
-            "LambdaExecutionRoleArn": "string"
-        }
+  "api": {
+    "apiab9b8614": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
     }
+  },
+  "auth": {
+    "cybertoolwebsite82b62747": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "cybertoolwebsite430c7922": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
 }
