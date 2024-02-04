214fd66707aeb301e768cf1b2e59be6147b9dd3d
Jeremiah Webb <illusion173@hotmail.com>
2024-02-03 23:08:45 -0500

    Updated Cargo.lock for both workspace and getsingualrtooldataLambdaUpdate, will rewrite it in python tho for now

--- code/lambda/update_report_lambdas.sh
+++ code/lambda/update_report_lambdas.sh
@@ -6,32 +6,32 @@ pushd $SCRIPT_DIR
 
 pushd ./reportcybertool
 cargo lambda build --release
-cargo lambda deploy -a LatestVersion
+cargo lambda deploy -a LatestVersion reportcybertool
 popd
 
 pushd ./reportcreatepresignedurl
 cargo lambda build --release
-cargo lambda deploy -a LatestVersion
+cargo lambda deploy -a LatestVersion reportcreatepresignedurl
 popd
 
 pushd ./beginreportcybertool
 cargo lambda build --release
-cargo lambda deploy -a LatestVersion
+cargo lambda deploy -a LatestVersion beginreportcybertool
 popd
 
 pushd ./getreportlist
 cargo lambda build --release
-cargo lambda deploy -a LatestVersion
+cargo lambda deploy -a LatestVersion getreportlist
 popd
 
 pushd ./getAllTools
 cargo lambda build --release
-cargo lambda deploy -a LatestVersion
+cargo lambda deploy -a LatestVersion getAllTools
 popd
 
 pushd ./getsingulartooldata
 cargo lambda build --release
-cargo lambda deploy -a LatestVersion
+cargo lambda deploy -a LatestVersion getsingulartooldata
 popd
 
 popd
