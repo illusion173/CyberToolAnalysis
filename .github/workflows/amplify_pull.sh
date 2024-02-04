167ba223c8c6ee487a1a55ca4ef6cf2bddca0990
Troy Neubauer <troyneubauer@gmail.com>
2024-02-04 17:41:47 -0500

    CI #6

---
+++ .github/workflows/amplify_pull.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+IFS='|'
+
+# Setup args so amplify can run headlessly in ci
+
+REACTCONFIG="{\
+\"SourceDir\":\"src\",\
+\"DistributionDir\":\"build\",\
+\"BuildCommand\":\"npm run-script build\",\
+\"StartCommand\":\"npm run-script start\"\
+}"
+AWSCLOUDFORMATIONCONFIG="{\
+\"configLevel\":\"project\",\
+\"useProfile\":false,\
+\"profileName\":\"default\",\
+\"accessKeyId\":\"$AWS_ACCESS_KEY_ID\",\
+\"secretAccessKey\":\"$AWS_SECRET_ACCESS_KEY\",\
+\"region\":\"$AWS_REGION\"\
+}"
+AMPLIFY="{\
+\"projectName\":\"CyberToolWebsite\",\
+\"appId\":\"dqijt2r7sg70n\",\
+\"envName\":\"main\",\
+\"defaultEditor\":\"code\"\
+}"
+FRONTEND="{\
+\"frontend\":\"javascript\",\
+\"framework\":\"react\",\
+\"config\":$REACTCONFIG\
+}"
+PROVIDERS="{\
+\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
+}"
+
+amplify pull \
+--amplify $AMPLIFY \
+--frontend $FRONTEND \
+--providers $PROVIDERS \
+--yes
