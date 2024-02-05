ad143c7b484cd50509bfcc07b0fbebe59eafaabc
Jeremiah Webb <illusion173@hotmail.com>
2024-02-04 22:36:18 -0500

    Updated API endpoint.

--- frontend/src/ReportList.js
+++ frontend/src/ReportList.js
@@ -19,7 +19,7 @@ const ReportList = () => {
     const jwt = await fetchJwt();
     const username = await getUserId();
     try {
-      const apiName = "apiab9b8614";
+      const apiName = "apib5cf8c70";
       const path = "/getReportListForUser";
 
       const headers = {
@@ -46,7 +46,7 @@ const ReportList = () => {
   const fetchPreSignedUrl = async (report_id) => {
     const jwt = await fetchJwt();
     try {
-      const apiName = "apiab9b8614";
+      const apiName = "apib5cf8c70";
       const path = "/requestpresignedurl";
 
       const headers = {
