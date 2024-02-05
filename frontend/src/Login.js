f1b72f6fc9c82434ab9b110672edb5cc24890d24
meholico <92179533+meholico@users.noreply.github.com>
2024-02-05 11:46:19 -0500

    Add files via upload
    Added forgot password page files

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Auth } from "aws-amplify"; // Import Auth from AWS Amplify
-import awsExports from "./aws-exports";
+import { Auth } from 'aws-amplify'; // Import Auth from AWS Amplify
 
-import { Amplify } from "aws-amplify";
-import "./Login.css";
+import awsExports from "./aws-exports";
+import { Amplify } from 'aws-amplify';
 
 Amplify.configure(awsExports);
 
@@ -17,10 +16,10 @@ function Login() {
         try {
             const user = await Auth.signIn(username, password);
           //  console.log(user); // You can remove this after confirming it works
-            navigate("/Questionnaire"); // Navigate to the dashboard after successful sign in
+            navigate("/dashboard"); // Navigate to the dashboard after successful sign in
         } catch (error) {
-            console.log("error signing in", error);
-            alert("Incorrect username/password"); // Display an error message
+            console.log('error signing in', error);
+            alert('Incorrect username/password'); // Display an error message
         }
     };
 
@@ -28,10 +27,9 @@ function Login() {
         navigate("/signup");
     };
 
-    const handleGuestClick = () => {
-        navigate ("/Dashboard")
-    }
-
+    const handleForgotClick = () => {
+        navigate("/forgotpassword");
+    };
     const handleSubmit = (e) => {
         e.preventDefault();
         handleSignIn(); // Call the handleSignIn function here
@@ -41,20 +39,16 @@ function Login() {
         <div className="login-form">
             <h2 className="login-message">Welcome Back!</h2>
             <form onSubmit={handleSubmit}>
-                <label className="login-label" htmlFor="username">
-                    Username:
-                </label>
+                <label className="login-label" htmlFor="username">Username:</label>
                 <input
                     className="login-input"
                     type="text"
-                    id="username"
+                    id='username'
                     name="username"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                 />
-                <label className="login-label" htmlFor="password">
-                    Password:
-                </label>
+                <label className="login-label" htmlFor="password">Password:</label>
                 <input
                     className="login-input"
                     type="password"
@@ -63,13 +57,10 @@ function Login() {
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 />
-                <button className="login-button" type="submit">
-                    Login
-                </button>
+                <button className="login-message" type="button" onClick={handleForgotClick}>Forgot password?</button>
+                <button className="login-button" type="submit">Login</button>
                 <p className="login-message">Don't have an account?</p>
                 <button type="button" className="signup-button" onClick={handleSignupClick}>Sign Up</button>
-                <p className = "login-message"> or</p>
-                <button type="button" className="guest-button" onClick = {handleGuestClick}>Continue as Guest</button>
             </form>
         </div>
     );
