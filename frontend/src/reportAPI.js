2232b35ffa4c56af5a02d62107b0665adc148b3b
Jeremiah Webb <illusion173@hotmail.com>
2024-02-15 13:15:24 -0500

    API Cleanup - NAVBAR

---
+++ frontend/src/reportAPI.js
@@ -0,0 +1,59 @@
+import { fetchJwt, getUserId } from "./helperFunctionsForUserAPI.js";
+import { API } from "aws-amplify";
+
+export async function fetchPreSignedUrl(report_id, file_name) {
+  const jwt = await fetchJwt();
+  try {
+    const apiName = "apic25cd3ea";
+    const path = "/requestpresignedurl";
+
+    const headers = {
+      Authorization: `Bearer ${jwt}`,
+    };
+
+    const requestBody = {
+      report_id: report_id,
+    };
+
+    const myInit = {
+      headers,
+      body: requestBody,
+    };
+
+    let presigned_url_data = await API.post(apiName, path, myInit);
+    var link = document.createElement("a");
+    link.href = presigned_url_data;
+    link.download = `${file_name}.pdf`; // Set the filename with the .pdf extension
+    link.click();
+  } catch (error) {
+    alert("Unable to retrieve presigned url");
+  }
+}
+
+export async function fetchReportList() {
+  const jwt = await fetchJwt();
+  const username = await getUserId();
+  try {
+    const apiName = "apic25cd3ea";
+
+    const path = "/getReportListForUser";
+
+    const headers = {
+      Authorization: `Bearer ${jwt}`,
+    };
+
+    const requestBody = {
+      user_identifier: `${username}`,
+    };
+
+    const myInit = {
+      headers,
+      body: requestBody,
+    };
+
+    let report_array = await API.post(apiName, path, myInit);
+    return report_array;
+  } catch (error) {
+    alert("Unable to retrieve report list");
+  }
+}
---
+++ frontend/src/reportTable.js
@@ -0,0 +1,53 @@
+import "./ReportList.css";
+import { fetchReportList, fetchPreSignedUrl } from "./reportAPI.js";
+import React, { useState, useEffect } from "react";
+
+function ReportTable() {
+  const [reportListArray, setReportListArray] = useState([]);
+
+  useEffect(() => {
+    console.log("HELLLLPPPP");
+    refreshClick();
+  }, []);
+
+  const refreshClick = async (e) => {
+    let report_list = await fetchReportList();
+    setReportListArray(report_list);
+  };
+
+  return (
+    <div>
+      <button onClick={() => refreshClick()} className="report-button-tools">
+        Refresh
+      </button>
+      <table className="report-styled-table">
+        <thead>
+          <tr>
+            <th>File Name</th>
+            <th>Date Created</th>
+            <th>Download</th>
+          </tr>
+        </thead>
+        <tbody>
+          {reportListArray.map((report, index) => (
+            <tr key={index}>
+              <td>{report.file_name}</td>
+              <td>{new Date(report.date_made).toLocaleDateString()}</td>
+              <td>
+                <button
+                  onClick={() =>
+                    fetchPreSignedUrl(report.report_id, report.file_name)
+                  }
+                >
+                  Download
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default ReportTable;
