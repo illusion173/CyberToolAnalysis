946f565698958745e5abdafc3bbce8b0293ccfe4
Troy Neubauer <troyneubauer@gmail.com>
2024-02-29 21:59:46 -0500

    use rust overlay from nix

--- flake.nix
+++ flake.nix
@@ -2,31 +2,34 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
-        in {
-          devShells.default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              # Frontend
-              nodejs
-              nodePackages.npm
-              awscli2
-              nodePackages.typescript
-              nodePackages.typescript-language-server
-              stdenv.cc.cc.lib
-              # Lambda
-              rustup
-              rust-analyzer
-              zig
-            ];
-          };
-        }
-      );
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Frontend
+            nodejs
+            nodePackages.npm
+            awscli2
+            nodePackages.typescript
+            nodePackages.typescript-language-server
+            stdenv.cc.cc.lib
+            # Lambda
+            rust
+            zig
+          ];
+        };
+      }
+    );
 }
